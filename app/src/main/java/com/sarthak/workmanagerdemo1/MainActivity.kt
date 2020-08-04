31e7cfe4323af899fa1baf594b3f537dea3aa17d
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-08-04 17:11:42 +0530

    Constraints set up.

--- app/src/main/java/com/sarthak/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/sarthak/workmanagerdemo1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.sarthak.workmanagerdemo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,8 +18,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOneTimeWorkRequest() {
+        // Constraints for the work manager request.
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
         // Creates a one time work request corresponding to the uploading work manager class.
-        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(UploadWorkManager::class.java).build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(UploadWorkManager::class.java)
+            .setConstraints(constraints)
+            .build()
         val workManager = WorkManager.getInstance(applicationContext)
         workManager.enqueue(oneTimeWorkRequest) // Enqueuing the work request using the work manager instance.
         // Getting work info by ID in the form of LiveData. We then observe these changes and display the workInfo's state name on the screen.
