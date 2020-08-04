08ebcb041de19cbd43c79a71a63431a62df59f2b
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-08-04 17:04:30 +0530

    nits.

--- app/src/main/java/com/sarthak/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/sarthak/workmanagerdemo1/MainActivity.kt
@@ -16,9 +16,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOneTimeWorkRequest() {
+        // Creates a one time work request corresponding to the uploading work manager class.
         val oneTimeWorkRequest = OneTimeWorkRequest.Builder(UploadWorkManager::class.java).build()
         val workManager = WorkManager.getInstance(applicationContext)
-        workManager.enqueue(oneTimeWorkRequest)
+        workManager.enqueue(oneTimeWorkRequest) // Enqueuing the work request using the work manager instance.
+        // Getting work info by ID in the form of LiveData. We then observe these changes and display the workInfo's state name on the screen.
         workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id).observe(this, Observer {
             textView.text = it.state.name
         })
