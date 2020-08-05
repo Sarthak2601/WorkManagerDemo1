9fa3a44174f064bc2e14c920e3dfbe51bfe50e70
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-08-05 15:14:28 +0530

    I/O Ops

--- app/src/main/java/com/sarthak/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/sarthak/workmanagerdemo1/MainActivity.kt
@@ -2,14 +2,16 @@ package com.sarthak.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val KEY_CONSTANT = "key_constant"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +20,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOneTimeWorkRequest() {
+        // Inputting data into the work manager.
+        val inputData: Data = Data.Builder()
+            .putInt(KEY_CONSTANT, 125)
+            .build()
         // Constraints for the work manager request.
         val constraints = Constraints.Builder()
             .setRequiresCharging(true)
@@ -26,12 +32,17 @@ class MainActivity : AppCompatActivity() {
         // Creates a one time work request corresponding to the uploading work manager class.
         val oneTimeWorkRequest = OneTimeWorkRequest.Builder(UploadWorkManager::class.java)
             .setConstraints(constraints)
+            .setInputData(inputData)
             .build()
+
         val workManager = WorkManager.getInstance(applicationContext)
         workManager.enqueue(oneTimeWorkRequest) // Enqueuing the work request using the work manager instance.
         // Getting work info by ID in the form of LiveData. We then observe these changes and display the workInfo's state name on the screen.
         workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id).observe(this, Observer {
             textView.text = it.state.name
+            // Receiving data from the work manager.
+            val receivedOutputValue = it.outputData.getString(UploadWorkManager.KEY_WORKER)
+            Toast.makeText(applicationContext, receivedOutputValue, Toast.LENGTH_SHORT).show()
         })
     }
 }
--- app/src/main/java/com/sarthak/workmanagerdemo1/UploadWorkManager.kt
+++ app/src/main/java/com/sarthak/workmanagerdemo1/UploadWorkManager.kt
@@ -2,16 +2,28 @@ package com.sarthak.workmanagerdemo1
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
 
 class UploadWorkManager(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    companion object{
+        const val KEY_WORKER = "key_worker"
+    }
     override fun doWork(): Result {
         return try {
-            for (i in 0..60) {
+            val inputData = inputData.getInt(MainActivity.KEY_CONSTANT, 0)
+            for (i in 0 until inputData) {
                 Log.i("WORK", "Uploading $i")
             }
-            Result.success()
+            val time = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val currentDate = time.format(Date())
+            val outputValue = Data.Builder().putString(KEY_WORKER, currentDate).build()
+
+            Result.success(outputValue)
         } catch (exception: Exception) {
             exception.printStackTrace()
             Result.failure()
